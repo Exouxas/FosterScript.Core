2cab90b93abeacc8a109cb7268356dc3411b7837
Christoffer Aske <exouxas@gmail.com>
2021-11-23 15:21:36 +0100

    Created an abstract sense class

---
+++ FosterScript/Senses/Sense.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using FosterScriptLib.Neurons;
+
+namespace FosterScriptLib.Senses
+{
+    public abstract class Sense : Neuron
+    {
+        public abstract Creature Parent { get; }
+    }
+}
