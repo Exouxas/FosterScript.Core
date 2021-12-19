d9a4e11a027286ff78ae699fa831773bb08afb1f
Christoffer Aske <exouxas@gmail.com>
2021-12-19 18:40:35 +0100

    Updated comment

--- FosterScript/Senses/ISensor.cs
+++ FosterScript/Senses/ISensor.cs
@@ -4,6 +4,7 @@ namespace FosterScriptLib.Senses
 {
     public interface ISensor
     {
-        // Can this stay empty?
+        // This is a marker interface.
+        // There might be a better way to do this, but this is how I've decided to do it so far.
     }
 }
