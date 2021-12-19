477d4898d4dbbaaa2d82d4a352a39366dd3d1973
Christoffer Aske <exouxas@gmail.com>
2021-12-19 18:36:11 +0100

    Did stuff

--- FosterScript/Senses/Sense.cs
+++ FosterScript/Senses/Sense.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-using FosterScriptLib.Neurons;
+﻿using FosterScriptLib.Neurons;
 
 namespace FosterScriptLib.Senses
 {
     public abstract class Sense : Neuron
     {
-        public abstract Creature Parent { get; }
+        protected abstract ISensor Parent { get; }
     }
 }
