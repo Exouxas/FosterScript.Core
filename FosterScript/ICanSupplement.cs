4be787826199408c88fd97e853dacca50f52aba6
Christoffer Aske <exouxas@gmail.com>
2022-01-08 03:22:28 +0100

    Prepared interfaces for system remake

---
+++ FosterScript/ICanSupplement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FosterScriptLib
+{
+    internal interface ICanSupplement
+    {
+        public double Output { get; }
+    }
+}
