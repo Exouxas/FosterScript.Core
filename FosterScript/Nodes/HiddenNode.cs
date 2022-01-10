88bf660baf5ea36f2468a08cfd1eef28d885b12e
Christoffer Aske <exouxas@gmail.com>
2022-01-10 17:37:27 +0100

    Removed redundant abstract method

--- FosterScript/Nodes/HiddenNode.cs
+++ FosterScript/Nodes/HiddenNode.cs
@@ -9,7 +9,6 @@ namespace FosterScriptLib.Nodes
     public abstract class HiddenNode : InputNode, ICanAugment
     {
         public List<ICanSupplement> Inputs { get; }
-        protected override abstract double GetOutput();
 
         protected HiddenNode(string name, string description) : base(name, description)
         {
