15a575ba68ae07286b19a9ad3553245c39adb611
Exouxas <exouxas@gmail.com>
2023-05-24 00:06:02 +0200

    Create basic set of "smart" modules

---
+++ src/Examples/Modules/SmartModules/SmartEnergy.cs
@@ -0,0 +1,70 @@
+﻿using FosterScript.Core.Agents;
+using FosterScript.Core.NeuralNetwork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FosterScript.Examples.Modules.SmartModules
+{
+    internal class SmartEnergy : Module
+    {
+        #region "Inherited Properties"
+        public override string Name => "SmartEnergy";
+        public override int[] Version => new int[] { 1, 0, 0 };
+        #endregion
+
+        #region "Properties"
+        public double EnergyStored 
+        {
+            get
+            {
+                return _energyStored;
+            }
+            set
+            {
+                if(value <= 0)
+                {
+                    Body?.Kill();
+                }
+                _energyStored = value;
+            } 
+        }
+        private double _energyStored;
+        #endregion
+
+        #region "Private values"
+        private Brain brain = null;
+        #endregion
+
+        public SmartEnergy() : base()
+        {
+            Dependencies.Add("BasicBrain", new int[] { 1, 0, 0 });
+        }
+
+        public override void Initialize()
+        {
+            // Add brain to local value
+            brain = (Brain)DependencyReferences["BasicBrain"];
+
+            // Add input nodes to brain
+            InputNode storedEnergyNeuron = new("Energy stored", "Gives the hyperbolic tangent of the amount of stored energy", 0);
+            storedEnergyNeuron.OnRequestOutput += (object sender, InputNeuronEventArgs e) =>
+            {
+                e.Output = Math.Tanh(EnergyStored);
+            };
+            brain.SupplementingNodes.Add(storedEnergyNeuron);
+        }
+
+        public override void Think()
+        {
+            // Module only used as value storage
+        }
+
+        public override void Act()
+        {
+            // Module only used as value storage
+        }
+    }
+}
---
+++ src/Examples/Modules/SmartModules/SmartMovement2D.cs
@@ -0,0 +1,80 @@
+﻿using FosterScript.Core.Agents;
+using FosterScript.Core.NeuralNetwork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FosterScript.Examples.Modules.SmartModules
+{
+    internal class SmartMovement2D : Module
+    {
+        #region "Inherited Properties"
+        public override string Name => "SmartMovement2D";
+        public override int[] Version => new int[] { 1, 0, 0 };
+        #endregion
+
+        #region "Properties"
+        public double Speed { get; set; }
+        #endregion
+
+        #region "Private values"
+        private Brain brain = null;
+        private OutputNode outputSpeedNeuron = null;
+        private OutputNode movementXNeuron = null;
+        private OutputNode movementYNeuron = null;
+        #endregion
+
+        public SmartMovement2D() : base()
+        {
+            Dependencies.Add("SmartEnergy", new int[] { 1, 0, 0 });
+            Dependencies.Add("BasicBrain", new int[] { 1, 0, 0 });
+        }
+
+        public override void Initialize()
+        {
+            // Add brain to local value
+            brain = (Brain)DependencyReferences["BasicBrain"];
+
+            // Add input nodes to brain
+            InputNode inputSpeedNeuron = new("Speed", "Gives the hyperbolic tangent of the current speed", 0);
+            inputSpeedNeuron.OnRequestOutput += (object sender, InputNeuronEventArgs e) =>
+            {
+                e.Output = Math.Tanh(Speed);
+            };
+            brain.SupplementingNodes.Add(inputSpeedNeuron);
+
+            // Add output nodes to brain
+            outputSpeedNeuron = new("Speed", "Distance of movement");
+            brain.AugmentingNodes.Add(outputSpeedNeuron);
+
+            movementXNeuron = new("Movement X", "X direction of movement");
+            brain.AugmentingNodes.Add(movementXNeuron);
+
+            movementYNeuron = new("Movement Y", "Y direction of movement");
+            brain.AugmentingNodes.Add(movementYNeuron);
+        }
+        public override void Think()
+        {
+            
+        }
+        public override void Act()
+        {
+            // Move according to output neurons
+            Speed = outputSpeedNeuron.Result * 10;
+            float x = (float)movementXNeuron.Result;
+            float y = (float)movementYNeuron.Result;
+            float normalizedDistance = (float)Math.Sqrt(x * x + y * y);
+
+            // Normalize distance
+            x /= normalizedDistance;
+            y /= normalizedDistance;
+
+            // Exponentially more exhausting to move faster
+            ((Energy)DependencyReferences["SmartEnergy"]).EnergyStored -= Speed * Speed;
+            Body?.Move(new Vector3(x * (float)Speed, y * (float)Speed, 0));
+        }
+    }
+}
